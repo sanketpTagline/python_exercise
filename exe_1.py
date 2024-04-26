0a8e38f17dbb67f30b25c5aa6b70545a01f6673b
sanketpTagline <sanketp.tagline@gmail.com>
2024-04-26 14:52:27 +0530

    changes que C and que D

--- exe_1.py
+++ exe_1.py
@@ -2,31 +2,33 @@ numbers = [2, 4, 5, 2, 12, 44, 5, 1, 2, 3]
 
 #  A. Length of the list
 list_length = len(numbers)
-print("Length of the list:",list_length)
+
+print("Length of the list: ",list_length)
 
 # B. Display first 3 numbers
-print("First 3 numbers:",numbers[:3])
+print("First 3 numbers: ",numbers[:3])
 
 #  C. Display sum of odd numbers
-sum_odd_number = [num for num in numbers if num%2!=0]
-print("Sum of odd numbers: ",sum(sum_odd_number))
+def even_numbers(num):
+    if(num%2 != 0):
+        return True
+    else:
+        return False
 
-#  D. Number of duplicate numbers
-    
-duplicate_list =[]     
+evenNums = filter(even_numbers, numbers)
+print("Sum of odd numbers: ",sum(evenNums))
 
+#  D. Number of duplicate numbers   
+duplicate = set()
+
+for num in numbers:
+    n = numbers.count(num)
+    if n > 1:
+           duplicate.add(num) 
+           
+print("Number of duplicate number : ",len(duplicate))
  
-for i in range(0, len(numbers)):    
-    for j in range(i+1, len(numbers)):    
-        if(numbers[i] == numbers[j]):    
-            if numbers[i] not in duplicate_list:
-                duplicate_list.append(numbers[i])
-
-print("Number of duplicate numbers: ",len(duplicate_list))
-
-
 # E.Display list without duplicate numbers
-
 distinct_list = []
 
 for i in numbers:
