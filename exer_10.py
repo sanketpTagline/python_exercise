2be808c0469cc64702283c8ac8e42346b055d25d
sanketpTagline <sanketp.tagline@gmail.com>
2024-04-29 09:58:13 +0530

    quetion 3,4,5 done

---
+++ exer_10.py
@@ -0,0 +1,103 @@
+# __*__
+# _***_
+# *****
+# _***_
+# __*__
+
+
+# for i in range(0,3):
+#     # print(i)
+   
+#     for j in range(3,i+1,-1):
+#         print("_",end="")
+        
+#     for k in range(0,i+1):
+#         print("*",end="")
+        
+    
+#     print()
+
+# for i in range(1,4):
+    
+#     for j in range(3-i,0,-1):
+#         print("_",end="")
+        
+#     for k in range(i):
+#         print("* ",end="")
+    
+#     print()
+
+
+# print("\n")
+
+for i in range (1,4):
+    for j in range(0,i):
+        print(" "*(3-i),end=" ")
+    for k in range(0,i):
+        print("*",end=" ")
+    print("\n")
+        
+
+# h = int(input("please enter  :"))
+
+# for i in range(h):
+#     print(" "*(h-i), "*"*(i*2+1))
+# for i in range(h-2, -1, -1):
+#     print(" "*(h-i), "*"*(i*2+1))
+# n = 5
+ 
+
+# for a1 in range (0,n):
+#     for a2 in range (a1):
+#         print("*", end="")
+#     print()
+
+# for a1 in range (n,0,-1):
+#     for a2 in range (a1):
+#         print("*", end="")
+#     print()
+# *
+# **
+# *_* 
+# *__*
+# *****
+# for i in range(0,5):
+#     for j in range(0 ,i+1):
+#         if i==0 or j==0 or j== i or i == 4:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+
+# print("\n")
+
+# ***** 
+# *___*
+# *___*
+# *___*
+# *****
+
+# for i in range(0,5):
+#     for j in range(0,5):
+#         if i == 0 or j == 0 or i == 4 or j == 4:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+        
+#     print()
+    
+# print("\n")   
+
+# 1 
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
+# count = 1
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#          print(count,end=" ")
+#          count += 1
+        
+#     print()
+  
