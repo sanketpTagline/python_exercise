67e834f082197fc9388b806e53507bb2db43cbb2
sanketpTagline <sanketp.tagline@gmail.com>
2024-05-01 09:40:49 +0530

    give proper name convension

--- exer_11.py
+++ exer_11.py
@@ -1,26 +1,26 @@
 GP = [3, 6, 9, 12, 16, 18]
 AP =[2, 5, 8, 11, 15, 17]
 
-def checkAP(list):
-    a = list[0]
-    d = list[1]-list[0]
+def checkAP(ap_list):
+    a = ap_list[0]
+    d = ap_list[1]-ap_list[0]
     
-    for n in range(1,len(list)-1): 
-        if (list[n + 1] - list[n]) != (list[n-1] - list[n-2]):
-            print(list[n-1])
-            list[n+1] = a + ((n+1) * d)
+    for n in range(1,len(ap_list)-1): 
+        if (ap_list[n + 1] - ap_list[n]) != (ap_list[n-1] - ap_list[n-2]):
+            print(ap_list[n-1])
+            ap_list[n+1] = a + ((n+1) * d)
             
-    print(list)
+    print(ap_list)
 
-def checkGP(list):
-    a = list[0]
-    r = list[1] / list[0]
+def checkGP(gp_list):
+    a = gp_list[0]
+    r = gp_list[1] / gp_list[0]
     
-    for i in range(2,len(list)):
-        if (list[i] / list[i - 1] != r):
-            list[i] = int(a * r**(i))
+    for i in range(2,len(gp_list)):
+        if (gp_list[i] / gp_list[i - 1] != r):
+            gp_list[i] = int(a * r**(i))
                    
-    print(list)
+    print(gp_list)
 
 print(checkAP([2, 5, 8, 11, 15, 17]))    
 print(checkGP([1,2,4,8,16,30,64,128,254,510]))
